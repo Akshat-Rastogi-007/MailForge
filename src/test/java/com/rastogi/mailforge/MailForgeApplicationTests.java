3734af25597c04ec7224d06b16f6517c3ce30b30
Akshat Rastogi <akshatrastogi.jaipuriahardoi@gmail.com>
2025-07-07 09:35:02 +0530

    Initial Commit

---
+++ src/test/java/com/rastogi/mailforge/MailForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rastogi.mailforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
