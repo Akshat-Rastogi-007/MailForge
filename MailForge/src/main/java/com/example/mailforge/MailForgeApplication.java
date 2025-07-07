28264018633708ec7493f9eb71bbd1b06369b1cb
Akshat Rastogi <akshatrastogi.jaipuriahardoi@gmail.com>
2025-07-07 09:20:01 +0530

    Initial Commit

---
+++ MailForge/src/main/java/com/example/mailforge/MailForgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailForgeApplication.class, args);
+    }
+
+}
